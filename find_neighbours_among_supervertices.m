91cac27f2a54c21b04be1caee6942557fff82ba6
Salim Arslan <sarslancs@gmail.com>
2015-09-04 17:32:55 +0100

    Create find_neighbours_among_supervertices.m

---
+++ find_neighbours_among_supervertices.m
@@ -0,0 +1,34 @@
+function [ N ] = find_neighbours_among_supervertices( nSupervs, superLabels, corticalMask, faces )
+%FIND_NEIGHBOURS_AMONG_SUPERVERTICES Generates the binary neighbourhood
+%matrix for the supervertices
+%	Returns N, the [nSupervs x nSupervs] neighbourhood matrix, for the
+%	supervertices. This matrix is necessary to apply a spatially
+%	constrained hierarchical clustering algorithm on the supervertices if
+%	the merging is to be performed only between the adjacent supervertices.
+
+N = zeros(nSupervs,nSupervs);
+[ map32to29, map29to32 ] = mapSurfaces(corticalMask);
+
+for i = 1 : nSupervs    
+    maps = map29to32(superLabels == i);
+    idx = [cell2mat(arrayfun(@(x) find(faces(:,1) == x, 1,'first'), maps, 'UniformOutput', false));...
+            cell2mat(arrayfun(@(x) find(faces(:,2) == x, 1,'first'), maps, 'UniformOutput', false));...
+            cell2mat(arrayfun(@(x) find(faces(:,3) == x, 1,'first'), maps, 'UniformOutput', false))];
+    
+    nodes = unique(faces(idx,:));
+    
+    idx = [cell2mat(arrayfun(@(x) find(faces(:,1) == x, 1,'first'), nodes, 'UniformOutput', false));...
+            cell2mat(arrayfun(@(x) find(faces(:,2) == x, 1,'first'), nodes, 'UniformOutput', false));...
+            cell2mat(arrayfun(@(x) find(faces(:,3) == x, 1,'first'), nodes, 'UniformOutput', false))];        
+    
+    nodes = unique(faces(idx,:));
+    nidx = unique(superLabels(nonzeros(map32to29(nodes))));
+    nidx(nidx == i) = [];
+    N(i,nidx) = 1;  
+   
+end
+
+N = logical(N | N');
+
+
+
