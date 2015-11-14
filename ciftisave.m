ee0e0928bea2aaadaf034daddfd4cec61059c452
Salim Arslan <sarslancs@gmail.com>
2015-11-14 12:20:26 +0000

    Update comments.

--- ciftisave.m
+++ ciftisave.m
@@ -1,6 +1,6 @@
 function [ output_args ] = ciftisave(cifti,filename,caret7command)
-%Save a CIFTI file as a GIFTI external binary and then convert it to CIFTI.
-%Provided as part of the Human Connectome Project pipelines
+% Save a CIFTI file as a GIFTI external binary and then convert it to CIFTI.
+% Provided as part of the Human Connectome Project pipelines
 
 tic
 save(cifti,[filename '.gii'],'ExternalFileBinary')
